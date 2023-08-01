39954c6bbbee55a6d270faa224697086acb80913
Jack Robert <100371269+JackRobert94464@users.noreply.github.com>
2023-08-02 03:15:13 +0700

    minor change

---
+++ Foundation/dqn-driver.py
@@ -0,0 +1,32 @@
+# import the class
+import dqn
+# classical gym 
+import gym
+# instead of gym, import gymnasium 
+#import gymnasium as gym
+ 
+# create environment
+env=gym.make('CartPole-v1')
+ 
+# select the parameters
+gamma=1
+# probability parameter for the epsilon-greedy approach
+epsilon=0.1
+# number of training episodes
+# NOTE HERE THAT AFTER CERTAIN NUMBERS OF EPISODES, WHEN THE PARAMTERS ARE LEARNED
+# THE EPISODE WILL BE LONG, AT THAT POINT YOU CAN STOP THE TRAINING PROCESS BY PRESSING CTRL+C
+# DO NOT WORRY, THE PARAMETERS WILL BE MEMORIZED
+numberEpisodes=1000
+ 
+# create an object
+LearningQDeep=dqn.DeepQLearning(env,gamma,epsilon,numberEpisodes)
+# run the learning process
+LearningQDeep.trainingEpisodes()
+# get the obtained rewards in every episode
+LearningQDeep.sumRewardsEpisode
+ 
+#  summarize the model
+LearningQDeep.mainNetwork.summary()
+# save the model, this is important, since it takes long time to train the model 
+# and we will need model in another file to visualize the trained model performance
+LearningQDeep.mainNetwork.save("trained_model_temp.h5")
