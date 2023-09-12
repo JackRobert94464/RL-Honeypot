9dc7799642de02f6d074ffdbb30786913b9ba6dd
Jack Robert <100371269+JackRobert94464@users.noreply.github.com>
2023-09-12 09:02:55 +0700

    make some change to ddqn-driver

--- Foundation/DoubleDeepQN/ddqn-driver.py
+++ Foundation/DoubleDeepQN/ddqn-driver.py
@@ -4,6 +4,8 @@ import ddqn
 import gym
 # instead of gym, import gymnasium 
 #import gymnasium as gym
+
+import numpy as np
  
 # create environment
 env=gym.make('CartPole-v1')
@@ -30,3 +32,51 @@ LearningQDeep.mainNetwork.summary()
 # save the model, this is important, since it takes long time to train the model 
 # and we will need model in another file to visualize the trained model performance
 LearningQDeep.mainNetwork.save("ddqn_trained_model_temp.keras")
+
+
+
+# Visualizing after training
+
+# load the model
+loaded_model = LearningQDeep.mainNetwok,,,rk
+
+sumObtainedRewards=0
+# simulate the learned policy for verification
+ 
+ 
+# create the environment, here you need to keep render_mode='rgb_array' since otherwise it will not generate the movie
+env = gym.make("CartPole-v1",render_mode='rgb_array')
+# reset the environment
+(currentState,prob)=env.reset()
+ 
+# Wrapper for recording the video
+# https://gymnasium.farama.org/api/wrappers/misc_wrappers/#gymnasium.wrappers.RenderCollection
+# the name of the folder in which the video is stored is "stored_video"
+# length of the video in the number of simulation steps
+# if we do not specify the length, the video will be recorded until the end of the episode 
+# that is, when terminalState becomes TRUE
+# just make sure that this parameter is smaller than the expected number of 
+# time steps within an episode
+# for some reason this parameter does not produce the expected results, for smaller than 450 it gives OK results
+video_length=400
+# the step_trigger parameter is set to 1 in order to ensure that we record the video every step
+#env = gym.wrappers.RecordVideo(env, 'stored_video',step_trigger = lambda x: x == 1, video_length=video_length)
+env = gym.wrappers.RecordVideo(env, 'stored_video_ddqn', video_length=video_length)
+ 
+ 
+# since the initial state is not a terminal state, set this flag to false
+terminalState=False
+while not terminalState:
+    # get the Q-value (1 by 2 vector)
+    Qvalues=loaded_model.predict(currentState.reshape(1,4))
+    # select the action that gives the max Qvalue
+    action=np.random.choice(np.where(Qvalues[0,:]==np.max(Qvalues[0,:]))[0])
+    # if you want random actions for comparison
+    #action = env.action_space.sample()
+    # apply the action
+    (currentState, currentReward, terminalState,_,_) = env.step(action)
+    # sum the rewards
+    sumObtainedRewards+=currentReward
+ 
+env.reset()
+env.close()
