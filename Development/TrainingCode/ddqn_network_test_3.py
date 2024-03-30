5b931f648ed4a69b08eda2314fef179a74474116
Jack Robert <100371269+JackRobert94464@users.noreply.github.com>
2024-03-30 09:36:33 +0700

    3-input-sample

--- Development/TrainingCode/ddqn_network_test_3.py
+++ Development/TrainingCode/ddqn_network_test_3.py
@@ -2,13 +2,19 @@ from keras.layers import Input, Dense, Concatenate
 from keras.models import Model
 from keras.optimizers import RMSprop
 
+import visualkeras
+
 from ddqn_agent import DoubleDeepQLearning
+import misc
 
 # observable = trang thai quan sat duoc (default input)
 # epss = ma tran EPSS size nxn 
 # => tinh trung binh epss cua tung canh => tao ma tran nxn => tai moi vi tri (k, l) cua ma tran => epss(k, l) = trung binh epss cua canh (k, l)
 # ntpg = ma tran ntpg size nxn
 # => tao ma tran nxn => tai moi vi tri (k, l) cua ma tran => ntpg(k, l) = 1 neu co canh tu k den l, nguoc lai = 0
+# Reference:
+# https://stackoverflow.com/questions/66786787/pytorch-multiple-branches-of-a-model
+# https://stackoverflow.com/questions/33487097/feeding-neural-network-matrix-data-as-input
 
 class Network:
     def __init__(self, observable_dimension, epss_dimension, ntpg_dimension, action_dimension):
@@ -52,12 +58,26 @@ class Network:
         return model
 
 
+# Load the NTPG and HTPG dictionaries
+ntpg = misc.create_dictionary_ntpg(".\\Development\\TPG-Data\\ntpg_eval.csv")
+htpg = misc.create_dictionary_htpg(".\\Development\\TPG-Data\\htpg_eval.csv")
+
+# Load the topology param from TPGs
+deception_nodes = misc.get_deception_nodes()
+normal_nodes = misc.count_nodes(ntpg)
+first_parameter = misc.calculate_first_parameter(deception_nodes, normal_nodes)
+
+# calculate the number of possible combinations
+total_permutations = misc.calculate_permutation(normal_nodes, deception_nodes)
+
 # Sample values for the dimensions
-observable_dimension = 10
-epss_dimension = 100
-ntpg_dimension = 10
-action_dimension = 100
+observable_dimension = normal_nodes
+epss_dimension = normal_nodes*normal_nodes
+ntpg_dimension = normal_nodes*normal_nodes
+action_dimension = total_permutations
 
 # Create the network
 network = Network(observable_dimension, epss_dimension, ntpg_dimension, action_dimension)
 model = network.createNetwork()
+model.save("trio-net.keras")
+visualkeras.layered_view(model, legend=True).show()
